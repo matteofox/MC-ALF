a880113542e8128023b6a97616cd7a0c4876bf8f
mfossati <matteofox88@gmail.com>
2026-01-28 18:33:52 +0100

    Cleanup

--- build/lib/mcalf/__init__.py
+++
@@ -1,8 +0,0 @@
-try:
-    from ._version import __version__
-except(ImportError):
-    pass
-
-from . import routines
-
-__all__ = ['routines']
--- build/lib/mcalf/routines/__init__.py
+++
@@ -1,8 +0,0 @@
-try:
-    from ._version import __version__
-except(ImportError):
-    pass
-
-from . import hires_fitter
-
-__all__ = ['hires_fitter']
